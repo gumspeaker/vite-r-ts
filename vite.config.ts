0633dc8bbca05413a4188903e2f645a27a338204
gumspeaker <774910945@qq.com>
2024-12-17 10:35:38 +0800

    feat: save

--- vite.config.ts
+++ vite.config.ts
@@ -3,44 +3,48 @@ import react from "@vitejs/plugin-react";
 
 import path from "path";
 import { viteExternalsPlugin } from "vite-plugin-externals";
-
+const resolve = (...p) => path.resolve(__dirname, ...p);
+const getReactPath = (name) =>
+  resolve(
+    `./package/react/build/oss-stable/${name}/umd/${name}.development.js`
+  );
 export default defineConfig({
   plugins: [
-    react({ jsxRuntime: "classic" }),
+    react({ jsxRuntime: "automatic" }),
     viteExternalsPlugin({
       react: "React",
       "react-dom": "ReactDOM",
     }),
   ],
-  // resolve: {
-  //   alias: [
-  //     {
-  //       find: "react",
-  //       replacement: getReactPath("react"),
-  //     },
-  //     {
-  //       find: "react-dom",
-  //       replacement: getReactPath("react-dom"),
-  //     },
-  //     {
-  //       find: "react-dom/client",
-  //       replacement: getReactPath("react-dom/client"),
-  //     },
-  //     {
-  //       find: "scheduler",
-  //       replacement: getReactPath("scheduler"),
-  //     },
-  //     {
-  //       find: "react-cache",
-  //       replacement: getReactPath("react-cache"),
-  //     },
-  //     {
-  //       find: "log",
-  //       replacement: getReactPath("log"),
-  //     },
-  //   ],
-  // },
-  // optimizeDeps: {
-  //   include: ["react", "react-dom", "scheduler", "react-cache", "log"],
-  // },
+  resolve: {
+    alias: [
+      {
+        find: "react",
+        replacement: getReactPath("react"),
+      },
+      {
+        find: "react-dom",
+        replacement: getReactPath("react-dom"),
+      },
+      {
+        find: "react-dom/client",
+        replacement: getReactPath("react-dom/client"),
+      },
+      {
+        find: "scheduler",
+        replacement: getReactPath("scheduler"),
+      },
+      {
+        find: "react-cache",
+        replacement: getReactPath("react-cache"),
+      },
+      {
+        find: "log",
+        replacement: getReactPath("log"),
+      },
+    ],
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "scheduler", "react-cache", "log"],
+  },
 });
